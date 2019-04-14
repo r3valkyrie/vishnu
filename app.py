901855a267b4d99cf26e2573383e4eb366574a9c
valkyrie <valkyrie@r3valkyrie.com>
2019-04-13 20:41:04 -0400

    Adds initial quest management commands, adds postgres module for running PSQL queries

--- app.py
+++ app.py
@@ -4,12 +4,16 @@
 #
 # Discord bot that handles dice rolling and other things
 
-import discord, yaml, vroll
+import discord, yaml, vroll, pgsql
 from discord.ext import commands
 
 config = yaml.safe_load(open("config.yaml"))
 token = config['token']
 chan_whitelist = config['chan_whitelist']
+pg_connection = config['pg_connection']
+
+# Create PostgreSQL tables.
+pgsql.create_tables(pg_connection)
 
 description = """
 Vishnu, a multipurpose D&D bot.
@@ -17,13 +21,67 @@ Vishnu, a multipurpose D&D bot.
 
 bot = commands.Bot(command_prefix='!', description=description)
 
+
+"""
+QUEST-RELATED COMMANDS
+"""
+@bot.command()
+async def addquest(ctx, quest_tier, *desc):
+    """
+    Allows a DM to create a quest.
+
+    !addquest [TIER] [DESCRIPTION]
+    """
+    quest_desc = " ".join(desc)
+    creator = str(ctx.author)
+
+    pgsql.import_quest_data(pg_connection, quest_tier, quest_desc, creator)
+
+    print("Tier {} quest added by {}. Description: {}".format(quest_tier, str(ctx.author), quest_desc))
+    await ctx.send("Tier {} quest added by {}. Description: {}".format(quest_tier, str(ctx.author), quest_desc))
+
+@bot.command()
+async def getquest(ctx, quest_id):
+    """
+    Allows any user to retrieve a quest by it's ID.
+
+    !getquest [ID]
+    """
+
+    query_return = pgsql.retrieve_quest_data(pg_connection, quest_id)
+    await ctx.send("""
+    QUEST ID: {} \nTIER: {}\nDESCRIPTION {}\nCREATOR: {}
+    {}
+    """.format(query_return[0], query_return[1], query_return[2], query_return[3], '-' * 20))
+
+@bot.command()
+async def getallquests(ctx):
+    """
+    Allows a user to retrieve all available quests
+
+    !allquests
+    """
+
+    query_return = pgsql.retrieve_all_quests(pg_connection)
+    for row in query_return:
+        await ctx.send("""
+    QUEST ID: {} \nTIER: {}\nDESCRIPTION {}\nCREATOR: {}
+        {}
+        """.format(row[0], row[1], row[2], row[3], '-' * 20))
+
+
+"""
+DICE COMMANDS
+"""
 @bot.command()
 async def roll(ctx, *args):
     """
-    Rolls dice. Accepts argumenuch as 1d6+4.
+    Rolls dice.
+
+    !roll [NUM]d[FACES]+[NUM]
     """
 
-    if chan_whitelist is None: # If the whitelist is empy, run the function in any channel
+    if chan_whitelist is None: # If the whitelist is empy, run the roll function in any channel
         for x in args:
             print("!roll command recieved in channel ID " + str(ctx.channel.id))
             await ctx.send(vroll.roll(x))
---
+++ pgsql.py
@@ -0,0 +1,74 @@
+"""
+PostgreSQL module for vishnu.
+"""
+
+import yaml, psycopg2
+
+
+def create_tables(pg_connection):
+
+    conn = psycopg2.connect(
+        dbname=pg_connection['database'],
+        user=pg_connection['user'],
+        password=pg_connection['password'],
+        host=pg_connection['host'])
+    cur = conn.cursor()
+
+    # Make the table if it doesn't exist.
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS vishnu_quests
+    (id SERIAL PRIMARY KEY, tier INTEGER, description VARCHAR, creator VARCHAR, completed BOOLEAN);
+    """)
+    conn.commit()
+    cur.close()
+    conn.close()
+
+def import_quest_data(pg_connection, quest_tier, quest_desc, creator):
+    conn = psycopg2.connect(
+        dbname=pg_connection['database'],
+        user=pg_connection['user'],
+        password=pg_connection['password'],
+        host=pg_connection['host'])
+    cur = conn.cursor()
+
+    cur.execute("""
+    INSERT INTO vishnu_quests (tier, description, creator, completed)
+    VALUES (%s, %s, %s, False);
+    """,
+    (quest_tier, quest_desc, creator)
+    )
+    conn.commit()
+    conn.close()
+
+def retrieve_quest_data(pg_connection, quest_id):
+    conn = psycopg2.connect(
+        dbname=pg_connection['database'],
+        user=pg_connection['user'],
+        password=pg_connection['password'],
+        host=pg_connection['host'])
+    cur = conn.cursor()
+
+    cur.execute("""
+    SELECT * FROM vishnu_quests
+    WHERE id = %s and completed = 'f';
+    """, quest_id)
+
+    results = list(cur.fetchall()[0])
+
+    return results
+
+def retrieve_all_quests(pg_connection):
+    conn = psycopg2.connect(
+        dbname=pg_connection['database'],
+        user=pg_connection['user'],
+        password=pg_connection['password'],
+        host=pg_connection['host'])
+    cur = conn.cursor()
+
+    cur.execute("""
+    SELECT * FROM vishnu_quests
+    WHERE completed = 'f';
+    """)
+
+    results = cur.fetchall()
+    return results
